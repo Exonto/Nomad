99d02b78fd252f6a8683f569110764d4d81f4974
Tyler <tylersyme@gmail.com>
2017-05-22 10:43:31 -0700

    Removed forms

--- Nomad/Nomad/Program.cs
+++ Nomad/Nomad/Program.cs
@@ -4,22 +4,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace Nomad
 {
 	static class Program
 	{
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		[STAThread]
 		static void Main()
 		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new GameForm());
-
 			Initialize();
 		}
 
@@ -32,8 +23,6 @@ namespace Nomad
 			FileManager.DeleteLogFile();
 			FileManager.EnsureLogFile();
 
-			Clock c = new Clock(100);
-			c.Start();
 		}
 	}
 }
